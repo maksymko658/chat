f18f21e0a981defe8f5dbd545104fda8fae44ff2
maksymko658 <jmaksy@ukr.net>
2016-08-10 04:47:34 +0300

    actioncable

---
+++ app/jobs/message_relay_job.rb
@@ -0,0 +1,10 @@
+class MessageRelayJob < ApplicationJob
+  queue_as :default
+
+  def perform(message)
+  	ActionCable.server.broadcast "chatrooms:#{message.chatroom.id}",{
+  	message: MessageController.render(message),
+  	chatroom_id: message.chatroom_id
+  }
+  end
+end
